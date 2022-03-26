f8eaa91b2a6142d2fce57ed119ff2e73a3bc50bd
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-03-25 22:13:39 -0400

    added notes and ext. to use w/ vs code + creating optimal mongodb+gql setup

--- server/src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const graphqlHTTP = require('express-graphql')
-
-const schema = {
-    // schema for new db
-}
